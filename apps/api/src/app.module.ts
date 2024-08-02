f33e103651d8213b1bc9625ba7448f4eac9567e7
Maxim Kiselev <spl33t@ya.ru>
2024-08-02 19:01:17 +0300

    docker upd

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+/* import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { WorksModule } from './works/works.module';
 import { FilesManagmentModule } from './file-managment/files.module';
-
+ */
 @Module({
   imports: [
-    DatabaseModule,
-    UsersModule,
-    AuthModule,
-    WorksModule,
-    FilesManagmentModule
+    //DatabaseModule,
+    //UsersModule,
+    //AuthModule,
+    //WorksModule,
+    //FilesManagmentModule
   ],
   controllers: [AppController],
   providers: [AppService],
