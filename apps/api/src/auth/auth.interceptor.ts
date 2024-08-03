7b19e6c879c11a3fc0e3df5434c2e79307d89089
Maxim Kiselev <spl33t@ya.ru>
2024-08-03 12:06:46 +0300

    docker upd

--- apps/api/src/auth/auth.interceptor.ts
+++ apps/api/src/auth/auth.interceptor.ts
@@ -21,7 +21,7 @@ export class AuthInterceptor implements NestInterceptor {
 
       //https
       //res.cookie(AUTH_MODULE_CONSTANTS.ACCESS_TOKEN_KEY, at, { httpOnly: true, sameSite: "none", secure: true })
-      res.cookie(AUTH_MODULE_CONSTANTS.ACCESS_TOKEN_KEY, at, {  })
+      res.cookie(AUTH_MODULE_CONSTANTS.ACCESS_TOKEN_KEY, at, { httpOnly: true, sameSite: "none", secure: false })
 
       return data
     }))
